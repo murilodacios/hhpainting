a7f0ad237d57e876f1d713d83e47bffb2a267d43
Murilo Dácio <murilodacio@gmail.com>
2023-11-22 16:17:39 -0300

    Add new year

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -7,7 +7,7 @@ export function Benefits() {
     return (
         <>
             <Stack bg="#fff" id="about" p="4" fontFamily="Inter" color="#000">
-                <Stack maxW={1400} w="100%" margin="0 auto" justify="space-between" pb="8">
+                <Stack maxW={1400} w="100%" margin="0 auto" justify="space-between" pb="8" spacing="6">
 
                     <SimpleGrid columns={{ base: 1, md: 3 }} margin="0 auto" gap="4" >
 
@@ -28,9 +28,9 @@ export function Benefits() {
                                 Superior Aesthetic Quality
                             </Text>
                             <Stack p="8" bg="white" borderTopLeftRadius="6" shadow="sm">
-                                <Text>Painting professionals possess the skills and experience to ensure a uniform and high-quality application. 
-                                    This results in superior aesthetics, with vibrant colors and a refined finish, enhancing the visual appeal of 
-                                    the residence. 
+                                <Text>Painting professionals possess the skills and experience to ensure a uniform and high-quality application.
+                                    This results in superior aesthetics, with vibrant colors and a refined finish, enhancing the visual appeal of
+                                    the residence.
                                 </Text>
                             </Stack>
                         </Stack>
@@ -40,14 +40,67 @@ export function Benefits() {
                                 Long-Term Cost Savings
                             </Text>
                             <Stack p="8" bg="white" borderTopLeftRadius="6" shadow="sm">
-                                <Text>Despite the initial investment, professional painting can represent long-term savings. Quality materials and 
-                                    proper techniques extend the life of the paint, reducing the need for frequent touch-ups. 
+                                <Text>Despite the initial investment, professional painting can represent long-term savings. Quality materials and
+                                    proper techniques extend the life of the paint, reducing the need for frequent touch-ups.
                                     Potentially contributing to an increase in its market worth.
                                 </Text>
                             </Stack>
                         </Stack>
 
                     </SimpleGrid>
+
+                    <Stack position="relative">
+                        <Image src="/ano-novo-1.png" borderRadius="10" />
+
+                        <Stack
+                            position={{base: "relative", md: "absolute"}}
+                            bg="#F1F7FF"
+                            px="8"
+                            py="8"
+                            borderRadius="6"
+                            spacing="6"
+                            border="1px solid #F1F7FF"
+                            shadow="sm"
+                            justify="space-between"
+                            width={{base: "100%", md: "600px"}}
+                            left={{base: "0px", md: "30px"}}
+                            top={{base: "0px", md: "30px"}}
+                        >
+                            <Text fontSize={{base: "2xl", md: "4xl"}} fontWeight="500" lineHeight="2.4rem">
+                                The new year cannot be the same
+                            </Text>
+                            <Text lineHeight="1.6rem">
+                                Start the new year with your home renewed, painted, and with a fresh atmosphere.
+                                Let's transform your spaces, creating a color palette that reflects your personality and style.
+                                With our dedicated and professional team, we ensure not only vibrant walls but also a complete
+                                renovation, reshaping the environment for a new year filled with positive energy.
+                            </Text>
+
+                            <Link href="/quote">
+
+                                <HStack
+                                    px="8"
+                                    py="3"
+                                    borderRadius="6"
+                                    bg="white"
+                                    color="#00132B"
+                                    align="center"
+                                    justify="center"
+                                    cursor="pointer"
+                                    transition="0.3s all"
+                                    border="1px solid #eee"
+                                    _hover={{
+                                        bg: "blue.200",
+                                        color: "#00132B"
+                                    }}
+                                >
+                                    <Text fontSize={{ base: "md", md: "md" }}>Get a free quote now</Text>
+                                    <Icon as={RiArrowRightLine} />
+                                </HStack>
+
+                            </Link>
+                        </Stack>
+                    </Stack>
 
 
                 </Stack>
